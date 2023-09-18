24ffa3331e6de80cc7f4614c074e9766982a81dc
ArnavDalal <94788843+ArnavDalal@users.noreply.github.com>
2023-09-18 20:16:47 +0100

    Updated files

---
+++ app.js
@@ -0,0 +1,63 @@
+
+x = 0;
+y = 0
+function change_text() {
+    x= document.getElementById("annie").innerHTML = x+1;
+    alert("Click OK to confirm")
+}
+
+function change_ext() {
+    y= document.getElementById("bunny").innerHTML = y+1;
+    alert("Click OK to confirm")
+}
+// Check-list Selector
+function attribute_change1() {
+    let element = document.getElementById('li1');
+    element.style.color= "green";
+}
+
+function attribute_change2() {
+    let element = document.getElementById('li2');
+    element.style.color= "green";
+}
+
+function attribute_change3() {
+    let element = document.getElementById('li3');
+    element.style.color= "green";
+}
+
+function attribute_change4() {
+    let element = document.getElementById('li4');
+    element.style.color= "green";
+}
+
+function attribute_change5() {
+    let element = document.getElementById('li5');
+    element.style.color= "green";
+}
+
+function attribute_change6() {
+    let element = document.getElementById('li6');
+    element.style.color= "green";
+}
+
+function attribute_change7() {
+    let element = document.getElementById('li7');
+    element.style.color= "green";
+}
+
+function attribute_change8() {
+    let element = document.getElementById('li8');
+    element.style.color= "green";
+}
+
+function attribute_change9() {
+    let element = document.getElementById('li9');
+    element.style.color= "green";
+}
+
+function attribute_change0() {
+    let element = document.getElementById('li10');
+    element.style.color= "green";
+}
+// end of
